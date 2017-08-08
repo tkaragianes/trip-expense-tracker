b6347e642dbb8297b0e3a963ba01a159181634c1
Thomas Karagianes <thomas@karagianes.com>
2017-08-07 23:51:22 -0700

    Add app shell
    Refactor app to include app shell (fixed bottom nav menu, etc).

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,63 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <app-shell></app-shell>
   </div>
 </template>
 
 <script>
+import AppShell from './components/layout/AppShell';
+
 export default {
   name: 'app',
+  components: {
+    AppShell,
+  },
 };
 </script>
 
 <style>
-#app {
+/*#app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+}*/
+
+  * {
+    box-sizing: border-box;
+    font-family: -apple-system, BlinkMacSystemFont,
+    "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell",
+    "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+    outline-color: inherit;
+  }
+
+  html {
+    font-size: 16px;
+  }
+
+  .page-content {
+    margin: 30px 10px 10px 10px;
+  }
+
+  .page-title {
+    /*'Helvetica Neue', 'Montserrat';*/
+    font-weight: 900;
+    font-size: 2rem;
+    color: #000000;
+    margin: 10px 0px 20px 0px;
+    position: relative;
+  }
+
+  .page-title::after {
+    content: '';
+    background-color: #000000;
+    width: 80px;
+    height: 3px;
+    position: absolute;
+    bottom: -5px; left: 0px;
+  }
+
 </style>
---
+++ src/components/layout/AppShell.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id='app-shell'>
+    <nav>
+      <router-link to='/list'>
+        <button type="button" name="button" class="list-button">List</button>
+      </router-link>
+      <router-link to='/add'>
+        <button type="button" name="button" class="add-button">Add</button>
+      </router-link>
+      <router-link to='/send'>
+        <button type="button" name="button" class="send-button">Send</button>
+      </router-link>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {
+  name: 'app-shell',
+  data: function data() {
+    return {
+
+    };
+  },
+};
+</script>
+<style>
+  body {
+    margin: 0;
+  }
+
+  nav {
+    display: flex;
+    flex-direction: row;
+    position: fixed;
+    bottom: 0;
+    width: 100vw;
+    /*background-color: #4366ee;*/
+    border-top: 1px solid #dddddd;
+  }
+
+  nav button {
+    width: 33vw;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    height: 50px;
+    background-color: #ffffff;
+    font-size: 1rem;
+  }
+
+  nav button a {
+    text-decoration: none;
+    color: #000000;
+  }
+
+  .add-button {
+    border-left: 1px solid #dddddd;
+    border-right: 1px solid #dddddd;
+  }
+
+</style>
